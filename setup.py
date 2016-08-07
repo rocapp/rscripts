188a9d9299b15637bb87548dd6d53a5b7aac8deb
= <=>
2016-08-07 16:10:26 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+import os
+
 
 setup(name='rscripts',
       version='0.023',
-      description='quick scripts',
+      description='quick scripts for many different uses',
       author='Robbie Capps',
       author_email='rocapp@gmail.com',
       url='https://robcapps.com',
-      packages=['rscripts'],
-      package_dir={'rscripts': 'src/rscripts'}
+      packages=map(lambda path: os.path.join('src', path), find_packages(where='src'))
      )
